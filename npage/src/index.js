ff1c1d6733f039dcea34d6f717e8f7c868a14c8b
T. Rahaman <developer@gmail.com>
2019-12-25 04:11:35 +0600

    revert

--- npage/src/index.js
+++ npage/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 import WebFont from 'webfontloader';
 
 WebFont.load({
